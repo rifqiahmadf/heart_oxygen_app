4ce11edd54b40e27f37e5481ece0a0f943dd9174
M. Jilan Fawwaz <47166420+jilanfawwaz@users.noreply.github.com>
2022-11-24 04:44:41 +0800

    maps done

--- lib/pages/homepagescreen/homemap.dart
+++ lib/pages/homepagescreen/homemap.dart
@@ -31,7 +31,7 @@ class _HomeMapState extends State<HomeMap> {
       'assets/images/custommarkersmall2.png',
     );*/
 
-    var markerIcon = await BitmapDescriptor.fromBytes(
+    var markerIcon = BitmapDescriptor.fromBytes(
       // const ImageConfiguration(),
       await ImageSizer.getBytesFromAsset(
         'assets/images/custommarkersmall2.png',
@@ -44,6 +44,37 @@ class _HomeMapState extends State<HomeMap> {
       infoWindow: const InfoWindow(
         title: 'Lokasi Saat ini',
         snippet: 'Aplikasi HeartRate & Oxygen',
+      ),
+      icon: markerIcon,
+    );
+
+    _markers[id] = marker;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  _addMarkerRumahSakit(
+      String id, LatLng location, String title, String snippet) async {
+    //? kalau dari assets langsung tanpa ImageSizer gunakan fromassetsImages
+    /*var markerIcon = await BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(),
+      'assets/images/custommarkersmall2.png',
+    );*/
+
+    var markerIcon = BitmapDescriptor.fromBytes(
+      // const ImageConfiguration(),
+      await ImageSizer.getBytesFromAsset(
+        'assets/images/custommarkerrumahsakit.png',
+        200,
+      ),
+    );
+    var marker = Marker(
+      markerId: MarkerId(id),
+      position: location,
+      infoWindow: InfoWindow(
+        title: title,
+        snippet: snippet,
       ),
       icon: markerIcon,
     );
@@ -87,21 +118,39 @@ class _HomeMapState extends State<HomeMap> {
           )
         : Stack(
             children: [
-              GoogleMap(
-                initialCameraPosition: CameraPosition(
-                  target: currentLocation,
-                  zoom: 14,
-                ),
-                onMapCreated: (controller) {
-                  _mapController = controller;
-                  //! Google Maps 6 : tambahkan method addMaker ke dalam onMapCreated
-                  _addMarker(
-                    'id1',
-                    currentLocation,
-                  );
+              BlocBuilder<MapCubit, MapState>(
+                builder: (context, state) {
+                  if (state is MapSuccess) {
+                    return GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: currentLocation,
+                        zoom: 14,
+                      ),
+                      onMapCreated: (controller) {
+                        _mapController = controller;
+                        //! Google Maps 6 : tambahkan method addMaker ke dalam onMapCreated
+                        _addMarker(
+                          'id1',
+                          currentLocation,
+                        );
+                        for (var element in state.marker) {
+                          _addMarkerRumahSakit(
+                            element.id,
+                            LatLng(
+                              element.latitude,
+                              element.longitude,
+                            ),
+                            element.name,
+                            element.alamat,
+                          );
+                        }
+                      },
+                      //! Google Maps 5 : masukkan variabel set _markers ke parameter markers
+                      markers: _markers.values.toSet(),
+                    );
+                  }
+                  return const SizedBox();
                 },
-                //! Google Maps 5 : masukkan variabel set _markers ke parameter markers
-                markers: _markers.values.toSet(),
               ),
               SafeArea(
                 child: BlocBuilder<MapCubit, MapState>(
@@ -109,74 +158,93 @@ class _HomeMapState extends State<HomeMap> {
                     if (state is MapSuccess) {
                       return Padding(
                         padding: const EdgeInsets.only(top: 30, left: 20),
-                        child: ListView.builder(
-                          clipBehavior: Clip.none,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: state.marker.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Align(
-                              alignment: Alignment.topCenter,
-                              child: Container(
-                                width: 250,
-                                height: 100,
-                                margin: const EdgeInsets.only(
-                                  right: 15,
-                                ),
-                                decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.5),
-                                      blurRadius: 10,
-                                      spreadRadius: 2,
-                                      offset: const Offset(0, 6),
-                                    ),
-                                  ],
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(
-                                    width: 4,
-                                    color: cPurpleDarkColor,
-                                  ),
-                                  color: Colors.grey.shade100,
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(20),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Image.asset(
-                                        'assets/images/rumahsakitbackground.jpeg',
-                                        fit: BoxFit.cover,
-                                        height: 100,
-                                        width: 80,
-                                      ),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      Expanded(
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              state.marker[index].name,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: cTextPurpleMaps,
-                                            ),
-                                            Text(
-                                              state.marker[index].alamat,
-                                              overflow: TextOverflow.ellipsis,
-                                            ),
-                                          ],
+                        child: SizedBox(
+                          height: 110,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            clipBehavior: Clip.none,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: state.marker.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Align(
+                                alignment: Alignment.topCenter,
+                                child: InkWell(
+                                  onTap: () {
+                                    _mapController.animateCamera(
+                                      CameraUpdate.newLatLng(
+                                        LatLng(
+                                          state.marker[index].latitude,
+                                          state.marker[index].longitude,
                                         ),
                                       ),
-                                    ],
+                                    );
+                                  },
+                                  child: Container(
+                                    width: 250,
+                                    height: 100,
+                                    margin: const EdgeInsets.only(
+                                      right: 15,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.5),
+                                          blurRadius: 10,
+                                          spreadRadius: 2,
+                                          offset: const Offset(0, 6),
+                                        ),
+                                      ],
+                                      borderRadius: BorderRadius.circular(20),
+                                      border: Border.all(
+                                        width: 4,
+                                        color: cPurpleDarkColor,
+                                      ),
+                                      color: Colors.grey.shade100,
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Image.asset(
+                                            'assets/images/rumahsakitbackground.jpeg',
+                                            fit: BoxFit.cover,
+                                            height: 100,
+                                            width: 80,
+                                          ),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          Expanded(
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  state.marker[index].name,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: cTextPurpleMaps,
+                                                ),
+                                                Text(
+                                                  state.marker[index].alamat,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
                       );
                     }
